82426b4077d0fc14a35a3fede68b5a5a88cb147b
Yan Sun <tsun812@gmail.com>
2022-03-21 20:39:37 -0700

    refactor dropdown list items

--- src/components/Dropdownlistitem.js
+++ src/components/Dropdownlistitem.js
@@ -1,5 +1,5 @@
 import { MenuItem } from "@mui/material";
 
 export default function Dropdownlistitem(props) {
-  return <MenuItem value={props.name}>{props.name}</MenuItem>;
+  return <MenuItem value={props.value}>{props.value}</MenuItem>;
 }
